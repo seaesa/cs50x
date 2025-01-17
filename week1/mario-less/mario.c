02216fae68714252f4b104ab095929739ddf072f
ngoc hai <hairipi100@gmail.com>
2025-01-17 22:13:29 +0700

    fix week1

--- week1/mario-less/mario.c
+++ week1/mario-less/mario.c
@@ -4,21 +4,25 @@ int main(void)
 {
   int height;
   char matrix[8][10] = {
-      "       #",
-      "      ##",
-      "     ###",
-      "    ####",
-      "   #####",
-      "  ######",
-      " #######",
+      "#",
+      "##",
+      "###",
+      "####",
+      "#####",
+      "######",
+      "#######",
       "########",
   };
   do
   {
     height = get_int("Height: ");
-  } while (height < 0 || height > 8);
+  } while (height <= 0 || height > 8);
   for (int i = 0; i < height; i++)
   {
+    for (int j = height - i - 1; j > 0; j--)
+    {
+      printf(" ");
+    }
     printf("%s\n", matrix[i]);
   }
   return 0;
